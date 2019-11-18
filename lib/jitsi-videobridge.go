61059b6045a5ed054f6d989ca1ee8a0ec43c41b3
Tomohiro Taira <tomohiro.t@gmail.com>
2019-11-18 18:07:07 +0900

    Implement FetchMetrics

--- lib/jitsi-videobridge.go
+++ lib/jitsi-videobridge.go
@@ -1,7 +1,10 @@
 package mpjitsivideobridge
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
+	"net/http"
 	"strings"
 
 	mp "github.com/mackerelio/go-mackerel-plugin"
@@ -12,6 +15,62 @@ type JitsiVideobridgePlugin struct {
 	Prefix string
 	Host   string
 	Port   string
+}
+
+// Stats represents a statistics.
+//
+// https://github.com/jitsi/jitsi-videobridge/blob/master/doc/statistics.md
+// https://github.com/jitsi/jitsi-videobridge/blob/master/src/main/java/org/jitsi/videobridge/stats/VideobridgeStatistics.java
+type Stats struct {
+	Audiochannels                         float64   `json:"audiochannels"`
+	BitRateDownload                       float64   `json:"bit_rate_download"`
+	BitRateUpload                         float64   `json:"bit_rate_upload"`
+	ConferenceSizes                       []float64 `json:"conference_sizes"`
+	Conferences                           float64   `json:"conferences"`
+	CPUUsage                              float64   `json:"cpu_usage"`
+	CurrentTimestamp                      string    `json:"current_timestamp"`
+	GracefulShutdown                      bool      `json:"graceful_shutdown"`
+	JitterAggregate                       float64   `json:"jitter_aggregate"`
+	LargestConference                     float64   `json:"largest_conference"`
+	LossRateDownload                      float64   `json:"loss_rate_download"`
+	LossRateUpload                        float64   `json:"loss_rate_upload"`
+	PacketRateDownload                    float64   `json:"packet_rate_download"`
+	PacketRateUpload                      float64   `json:"packet_rate_upload"`
+	Participants                          float64   `json:"participants"`
+	Region                                string    `json:"region"`
+	RelayID                               string    `json:"relay_id"`
+	RtpLoss                               float64   `json:"rtp_loss"`
+	RttAggregate                          float64   `json:"rtt_aggregate"`
+	Threads                               float64   `json:"threads"`
+	TotalBytesReceived                    float64   `json:"total_bytes_received"`
+	TotalBytesReceivedOcto                float64   `json:"total_bytes_received_octo"`
+	TotalBytesSent                        float64   `json:"total_bytes_sent"`
+	TotalBytesSentOcto                    float64   `json:"total_bytes_sent_octo"`
+	TotalColibriWebSocketMessagesReceived float64   `json:"total_colibri_web_socket_messages_received"`
+	TotalColibriWebSocketMessagesSent     float64   `json:"total_colibri_web_socket_messages_sent"`
+	TotalConferenceSeconds                float64   `json:"total_conference_seconds"`
+	TotalConferencesCompleted             float64   `json:"total_conferences_completed"`
+	TotalConferencesCreated               float64   `json:"total_conferences_created"`
+	TotalDataChannelMessagesReceived      float64   `json:"total_data_channel_messages_received"`
+	TotalDataChannelMessagesSent          float64   `json:"total_data_channel_messages_sent"`
+	TotalFailedConferences                float64   `json:"total_failed_conferences"`
+	TotalIceFailed                        float64   `json:"total_ice_failed"`
+	TotalIceSucceeded                     float64   `json:"total_ice_succeeded"`
+	TotalIceSucceededTCP                  float64   `json:"total_ice_succeeded_tcp"`
+	TotalLossControlledParticipantSeconds float64   `json:"total_loss_controlled_participant_seconds"`
+	TotalLossDegradedParticipantSeconds   float64   `json:"total_loss_degraded_participant_seconds"`
+	TotalLossLimitedParticipantSeconds    float64   `json:"total_loss_limited_participant_seconds"`
+	TotalMemory                           float64   `json:"total_memory"`
+	TotalPacketsDroppedOcto               float64   `json:"total_packets_dropped_octo"`
+	TotalPacketsReceived                  float64   `json:"total_packets_received"`
+	TotalPacketsReceivedOcto              float64   `json:"total_packets_received_octo"`
+	TotalPacketsSent                      float64   `json:"total_packets_sent"`
+	TotalPacketsSentOcto                  float64   `json:"total_packets_sent_octo"`
+	TotalPartiallyFailedConferences       float64   `json:"total_partially_failed_conferences"`
+	TotalParticipants                     float64   `json:"total_participants"`
+	UsedMemory                            float64   `json:"used_memory"`
+	Videochannels                         float64   `json:"videochannels"`
+	Videostreams                          float64   `json:"videostreams"`
 }
 
 // MetricKeyPrefix returns prefix of Jitsi Videobridge metrics
@@ -33,17 +92,113 @@ func (p JitsiVideobridgePlugin) GraphDefinition() map[string]mp.Graphs {
 	}
 }
 
-// FetchMetrics fetches metrics from Jitsi Videobridge Colibri REST interface
+// FetchMetrics fetches metrics from Jitsi Videobridge Colibri REST float64erface
 func (p JitsiVideobridgePlugin) FetchMetrics() (map[string]float64, error) {
+	url := fmt.Sprintf("http://%v:%v/colibri/stats", p.Host, p.Port)
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get a request: %w", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("bad response code: %v", res.StatusCode)
+	}
+
+	stats := Stats{}
+	if err = json.NewDecoder(res.Body).Decode(&stats); err != nil {
+		return nil, fmt.Errorf("failed to decode a responded JSON. %w", err)
+	}
+
+	return transformStatsToMetrics(&stats), nil
+}
+
+func transformStatsToMetrics(s *Stats) map[string]float64 {
 	metrics := make(map[string]float64)
-	return metrics, nil
+
+	// CPU / Memory
+	metrics["cpu_usage"] = s.CPUUsage
+	metrics["total_memory"] = s.TotalMemory
+	metrics["used_memory"] = s.UsedMemory
+
+	// Java
+	metrics["threads"] = s.Threads
+
+	// Conferences
+	metrics["conferences"] = s.Conferences
+	metrics["total_conferences_completed"] = s.TotalConferencesCompleted
+	metrics["total_conferences_created"] = s.TotalConferencesCreated
+	metrics["total_failed_conferences"] = s.TotalFailedConferences
+	metrics["total_partially_failed_conferences"] = s.TotalPartiallyFailedConferences
+
+	// Conference Lengths
+	metrics["total_conference_seconds"] = s.TotalConferenceSeconds
+	metrics["total_loss_controlled_participant_seconds"] = s.TotalLossControlledParticipantSeconds
+	metrics["total_loss_degraded_participant_seconds"] = s.TotalLossDegradedParticipantSeconds
+	metrics["total_loss_limited_participant_seconds"] = s.TotalLossLimitedParticipantSeconds
+
+	// Participants
+	metrics["participants"] = s.Participants
+	metrics["total_participants"] = s.TotalParticipants
+	metrics["largest_conference"] = s.LargestConference
+
+	// Channels / Streams
+	metrics["audiochannels"] = s.Audiochannels
+	metrics["videochannels"] = s.Videochannels
+	metrics["videostreams"] = s.Videostreams
+
+	// ICE connection total statuses
+	metrics["total_ice_succeeded"] = s.TotalIceSucceeded
+	metrics["total_ice_succeeded_tcp"] = s.TotalIceSucceededTCP
+	metrics["total_ice_failed"] = s.TotalIceFailed
+
+	// Jitter (Experimentail)
+	metrics["jitter_aggregate"] = s.JitterAggregate
+
+	// RTT
+	metrics["rtt_aggregate"] = s.RttAggregate
+
+	// Videobridge bit rate download / upload
+	metrics["bit_rate_download"] = s.BitRateDownload
+	metrics["bit_rate_upload"] = s.BitRateUpload
+
+	// Videobridge packet rate download / upload
+	metrics["packet_rate_download"] = s.PacketRateDownload
+	metrics["packet_rate_upload"] = s.PacketRateUpload
+
+	// RTP packet loss rate download / upload
+	metrics["loss_rate_download"] = s.LossRateDownload
+	metrics["loss_rate_upload"] = s.LossRateUpload
+
+	// Bytes received / sent
+	metrics["total_bytes_received"] = s.TotalBytesReceived
+	metrics["total_bytes_received_octo"] = s.TotalBytesReceivedOcto
+	metrics["total_bytes_sent"] = s.TotalBytesSent
+	metrics["total_bytes_sent_octo"] = s.TotalBytesSentOcto
+
+	// Packet total received / sent / dropped
+	metrics["total_packets_received"] = s.TotalPacketsReceived
+	metrics["total_packets_received_octo"] = s.TotalPacketsReceivedOcto
+	metrics["total_packets_sent"] = s.TotalPacketsSent
+	metrics["total_packets_sent_octo"] = s.TotalPacketsSentOcto
+	metrics["total_packets_dropped_octo"] = s.TotalPacketsDroppedOcto
+
+	// Datachannel messages total received / sent
+	metrics["total_data_channel_messages_received"] = s.TotalDataChannelMessagesReceived
+	metrics["total_data_channel_messages_sent"] = s.TotalDataChannelMessagesSent
+
+	// Colibri WebSocket messages total received / sent
+	metrics["total_colibri_web_socket_messages_received"] = s.TotalColibriWebSocketMessagesReceived
+	metrics["total_colibri_web_socket_messages_sent"] = s.TotalColibriWebSocketMessagesSent
+
+	return metrics
 }
 
 // Do the plugin
 func Do() {
 	optPrefix := flag.String("metric-key-prefix", "jitsi-videobridge", "Metric key prefix")
-	optHost := flag.String("host", "127.0.0.1", "Hostname or IP address of Jitsi Videobridge Colibri REST interface")
-	optPort := flag.String("port", "80", "Port of Jitsi Videobridge Colibri REST interface")
+	optHost := flag.String("host", "127.0.0.1", "Hostname or IP address of Jitsi Videobridge Colibri REST float64erface")
+	optPort := flag.String("port", "80", "Port of Jitsi Videobridge Colibri REST float64erface")
 	optTempfile := flag.String("tempfile", "", "Temp file name")
 	flag.Parse()
 
--- lib/jitsi-videobridge_test.go
+++ lib/jitsi-videobridge_test.go
@@ -1,23 +1,38 @@
 package mpjitsivideobridge
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
 	"testing"
 )
 
 var (
+	mux    *http.ServeMux
+	server *httptest.Server
 	plugin JitsiVideobridgePlugin
 )
 
 func setup() {
+	mux = http.NewServeMux()
+	server = httptest.NewServer(mux)
+	u, _ := url.Parse(server.URL)
 	plugin = JitsiVideobridgePlugin{
 		Prefix: "jitsi-videobridge",
-		Host:   "127.0.0.1",
-		Port:   "80",
+		Host:   u.Hostname(),
+		Port:   u.Port(),
 	}
+}
+
+func teardown() {
+	server.Close()
 }
 
 func TestMetricKeyPrefix(t *testing.T) {
 	setup()
+	defer teardown()
 
 	want := "jitsi-videobridge"
 	got := plugin.MetricKeyPrefix()
@@ -32,5 +47,137 @@ func TestGraphDefinition(t *testing.T) {
 }
 
 func TestFetchMetrics(t *testing.T) {
-	t.Skip()
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/colibri/stats", func(w http.ResponseWriter, r *http.Request) {
+		// Set response headers
+		w.Header().Set("Content-Type", "application/json")
+
+		// Set 200 OK as HTTP status code.
+		w.WriteHeader(http.StatusOK)
+
+		// Set response body
+		// This example is response body that from https://github.com/jitsi/jitsi-videobridge/blob/master/doc/statistics.md
+		fmt.Fprintln(w, `
+		{
+			"audiochannels": 0,
+			"bit_rate_download": 0,
+			"bit_rate_upload": 0,
+			"conference_sizes": [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+			"conferences": 0,
+			"cpu_usage": 0.2358490566037736,
+			"current_timestamp": "2019-03-14 11:02:15.184",
+			"graceful_shutdown": false,
+			"jitter_aggregate": 0,
+			"largest_conference": 0,
+			"loss_rate_download": 0,
+			"loss_rate_upload": 0,
+			"packet_rate_download": 0,
+			"packet_rate_upload": 0,
+			"participants": 0,
+			"region": "eu-west-1",
+			"relay_id": "10.0.0.5:4096",
+			"rtp_loss": 0,
+			"rtt_aggregate": 0,
+			"threads": 59,
+			"total_bytes_received": 257628359,
+			"total_bytes_received_octo": 0,
+			"total_bytes_sent": 257754048,
+			"total_bytes_sent_octo": 0,
+			"total_colibri_web_socket_messages_received": 0,
+			"total_colibri_web_socket_messages_sent": 0,
+			"total_conference_seconds": 470,
+			"total_conferences_completed": 1,
+			"total_conferences_created": 1,
+			"total_data_channel_messages_received": 602,
+			"total_data_channel_messages_sent": 600,
+			"total_failed_conferences": 0,
+			"total_ice_failed": 0,
+			"total_ice_succeeded": 2,
+			"total_ice_succeeded_tcp": 0,
+			"total_loss_controlled_participant_seconds": 847,
+			"total_loss_degraded_participant_seconds": 1,
+			"total_loss_limited_participant_seconds": 0,
+			"total_memory": 8257,
+			"total_packets_dropped_octo": 0,
+			"total_packets_received": 266644,
+			"total_packets_received_octo": 0,
+			"total_packets_sent": 266556,
+			"total_packets_sent_octo": 0,
+			"total_partially_failed_conferences": 0,
+			"total_participants": 2,
+			"used_memory": 4404,
+			"videochannels": 0,
+			"videostreams": 0
+		  }
+		`)
+	})
+
+	stats, err := plugin.FetchMetrics()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if stats == nil {
+		t.Errorf("empty response: %#v", stats)
+	}
+
+	got := stats
+	want := map[string]float64{
+		"audiochannels":     0,
+		"bit_rate_download": 0,
+		"bit_rate_upload":   0,
+		// "conference_sizes": [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+		"conferences": 0,
+		"cpu_usage":   0.2358490566037736,
+		// "current_timestamp": "2019-03-14 11:02:15.184",
+		// "graceful_shutdown": false,
+		"jitter_aggregate":     0,
+		"largest_conference":   0,
+		"loss_rate_download":   0,
+		"loss_rate_upload":     0,
+		"packet_rate_download": 0,
+		"packet_rate_upload":   0,
+		"participants":         0,
+		// "region": "eu-west-1",
+		// "relay_id": "10.0.0.5:4096",
+		// "rtp_loss": 0,
+		"rtt_aggregate":                              0,
+		"threads":                                    59,
+		"total_bytes_received":                       257628359,
+		"total_bytes_received_octo":                  0,
+		"total_bytes_sent":                           257754048,
+		"total_bytes_sent_octo":                      0,
+		"total_colibri_web_socket_messages_received": 0,
+		"total_colibri_web_socket_messages_sent":     0,
+		"total_conference_seconds":                   470,
+		"total_conferences_completed":                1,
+		"total_conferences_created":                  1,
+		"total_data_channel_messages_received":       602,
+		"total_data_channel_messages_sent":           600,
+		"total_failed_conferences":                   0,
+		"total_ice_failed":                           0,
+		"total_ice_succeeded":                        2,
+		"total_ice_succeeded_tcp":                    0,
+		"total_loss_controlled_participant_seconds":  847,
+		"total_loss_degraded_participant_seconds":    1,
+		"total_loss_limited_participant_seconds":     0,
+		"total_memory":                               8257,
+		"total_packets_dropped_octo":                 0,
+		"total_packets_received":                     266644,
+		"total_packets_received_octo":                0,
+		"total_packets_sent":                         266556,
+		"total_packets_sent_octo":                    0,
+		"total_partially_failed_conferences":         0,
+		"total_participants":                         2,
+		"used_memory":                                4404,
+		"videochannels":                              0,
+		"videostreams":                               0,
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Metrics got %#v, want %#v", got, want)
+	}
 }
